00f174af5900eaf79edb4297183a0f8bbbbbf0d0
abhic <abhich1607@gmail.com>
2022-09-21 14:52:48 +0530

    Website scaffold completed

---
+++ index.js
@@ -0,0 +1,25 @@
+function readCurrComp() {
+    let moreText = document.getElementById('p-read-current-company');
+    var btn = document.getElementById('btn-read-current-company');
+
+    if(moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btn.innerHTML = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "Read More";
+    }
+}
+
+function readInternComp() {
+    let moreText = document.getElementById('p-read-intern-company');
+    var btn = document.getElementById('btn-read-intern-company');
+
+    if(moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btn.innerHTML = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "Read More";
+    }
+}
